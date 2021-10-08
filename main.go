96fae9da747eb89c3b9ce69ff2bd0ab1b84a8014
Tatiana <tatyana@devindi.com>
2021-10-08 22:45:16 +0300

    rec

--- main.go
+++ main.go
@@ -8,46 +8,51 @@ import (
 
 func main() {
 	//read font art
-	fontFile := "shadow"
-	fContent, err := os.ReadFile(fontFile + ".txt")
+	fContent, err := os.ReadFile("standard.txt")
 	check(err)
 	fontData := string(fContent)
 	font := strings.Split(fontData, "\n")
-	//read file for reverse
-	textFile := "test00"
-	textContent, err := os.ReadFile(textFile + ".txt")
+	//read art file for reverse
+	textFile := os.Args[1][10:]
+	textContent, err := os.ReadFile(textFile)
 	check(err)
 	textData := string(textContent)
 	text := strings.Split(textData, "\n")
-	reverse(font, text, 0, 0, 1)
-	fmt.Println()
+
+	if len(text) > 9 {
+		for i := 0; i < len(text)-1; {
+
+			reverse(font, text[i:i+8], 0, 0, 1)
+			fmt.Println()
+			i = i + 9
+
+		}
+
+	} else {
+		reverse(font, text, 0, 0, 1)
+		fmt.Println()
+	}
 
 }
 func reverse(font []string, text []string, pos int, count int, start int) {
 	if pos != len(text[count]) {
 		l := len(font[start])
 		if pos+l <= len(text[count]) {
-			//fmt.Println("pos", pos, pos+l)
 			if count < 7 {
-				//fmt.Println("count", count)
 				if text[count][pos:l+pos] == font[start+count] {
-					//fmt.Println("if")
 					reverse(font, text, pos, count+1, start)
 				} else {
-					//fmt.Println("el")
 					reverse(font, text, pos, 0, start+9)
 				}
 			} else {
 				r := ((start - 1) / 9) + 32
 				fmt.Printf("%c", r)
-				//fmt.Println("newpos", pos+l)
 				reverse(font, text, pos+l, 0, 1)
 			}
 		} else {
 			reverse(font, text, pos, 0, start+9)
 		}
 	}
-
 }
 
 func check(e error) {
