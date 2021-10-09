edc9f3d18ac70c4e7051e717c5d3a74f23e9aba0
Tatiana <tatyana@devindi.com>
2021-10-09 19:33:27 +0300

    test file

---
+++ test.sh
@@ -0,0 +1,41 @@
+echo 'Try passing as arguments "--reverse example00.txt"'
+go run main.go "--reverse example00.txt"
+
+cat -e example00.txt
+echo 'Try passing as arguments --reverse=example00.txt'
+go run main.go --reverse=example00.txt
+
+
+cat -e example01.txt
+echo 'Try passing as arguments --reverse=example01.txt'
+go run main.go --reverse=example01.txt
+
+cat -e example02.txt
+echo 'Try passing as arguments --reverse=example02.txt'
+go run main.go --reverse=example02.txt
+
+cat -e example03.txt
+echo 'Try passing as arguments --reverse=example03.txt'
+go run main.go --reverse=example03.txt
+
+cat -e example04.txt
+echo 'Try passing as arguments --reverse=example04.txt'
+go run main.go --reverse=example04.txt
+
+cat -e example05.txt
+echo 'Try passing as arguments --reverse=example05.txt'
+go run main.go --reverse=example05.txt
+
+
+cat -e example06.txt
+echo 'Try passing as arguments --reverse=example06.txt'
+go run main.go --reverse=example06.txt
+
+cat -e example07.txt
+echo 'Try passing as arguments --reverse=example07.txt'
+go run main.go --reverse=example07.txt
+
+cat -e example08.txt
+echo 'Try passing as arguments --reverse=example08.txt'
+go run main.go --reverse=example08.txt
+
