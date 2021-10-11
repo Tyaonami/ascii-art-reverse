a493e06786006dca7a52828660db80678177f8c1
Tatiana <tatyana@devindi.com>
2021-10-11 20:13:48 +0300

    comments

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 		check(err)
 		textData := string(textContent)
 		text := strings.Split(textData, "\n")
+		//If the file contains more than one object ascii-art
 		if len(text) > 9 {
 			for i := 0; i < len(text)-1; {
 				if len(text[i]) > 0 {
@@ -40,34 +41,35 @@ func main() {
 	}
 
 }
+
+// pos - symbol in ascii-art file(input.txt). Count - count of lines in ascii-art file(input.txt). Start - number of line in font(standard.txt)
 func reverse(font []string, text []string, pos int, count int, start int) {
-	if pos != len(text[count]) {
-		l := len(font[start])
+	if pos != len(text[count]) { //if we are not finish reverse.
+		l := len(font[start]) // len of candite for research
 		if pos+l <= len(text[count]) {
 			if count < 7 {
-				if text[count][pos:l+pos] == font[start+count] {
-					reverse(font, text, pos, count+1, start)
+				if text[count][pos:l+pos] == font[start+count] { // if the font-line and line of ascii-art equal
+					reverse(font, text, pos, count+1, start) // compare the next line
 				} else {
-					reverse(font, text, pos, 0, start+9)
+					reverse(font, text, pos, 0, start+9) // if not equal we try the next symbol in font-file
 				}
 			} else {
-				r := ((start - 1) / 9) + 32
+				r := ((start - 1) / 9) + 32 // find and print the letter
 				fmt.Printf("%c", r)
-				reverse(font, text, pos+l, 0, 1)
+				reverse(font, text, pos+l, 0, 1) // we restart our counts for next reserch
 			}
 		} else {
 			reverse(font, text, pos, 0, start+9)
 		}
 	}
 }
-
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-// the last argument must be started from --output= and contains at least 1 character
+// the argument must be started from --reverse=
 func checkArgs() (bool, string) {
 	ok := true
 	s := ""
